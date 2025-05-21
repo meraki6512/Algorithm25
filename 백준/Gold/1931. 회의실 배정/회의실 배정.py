4a4d5764d328b769225c5e1ad63de0d0db5784cb
jiwoo_ya <jweun1@soongsil.ac.kr>
2025-05-21 07:06:56 -0700

    [Gold V] Title: 회의실 배정, Time: 2764 ms, Memory: 60360 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,32 @@
+# 1931 회의실 배정
+# 2초, 128MB
+
+# 1개의 회의실, N개의 회의(10^5), 각 회의 I(시작, 끝: 음이 아닌 int)
+# 겹치지 않는 최대 회의 개수 = ?
+
+# input
+N = int(input())
+info = [list(map(int, input().split())) for _ in range(N)]
+
+# solution
+# - 끝 순서대로 정렬하고 찾는다 (이걸로 먼저 해보자자)
+# - (끝-시작) 순서대로 정렬하고 찾는다
+info.sort(key = lambda x: (x[1], x[0])) # nlogn
+
+cur = info[0]
+take = [info[0]]
+
+for i, time in enumerate(info):
+
+    if i == 0:
+        continue
+
+    if cur[1] > time[0]:    # 현재 회의 끝 시각 > 새 회의 시작 시각이면
+        continue            # 회의 불가
+
+    else:
+        cur = time          # 회의 시작
+        take.append(time)   # 정답 배열에 넣음
+
+# output
+print(len(take))
