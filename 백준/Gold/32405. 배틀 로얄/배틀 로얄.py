e635c8b9393f9329c0219f088535af393d626d3c
jiwoo_ya <jweun1@soongsil.ac.kr>
2025-06-12 23:19:50 +0900

    [Gold IV] Title: 배틀 로얄, Time: 1888 ms, Memory: 62864 KB -BaekjoonHub

--- 백준/Gold/32405. 배틀 로얄/배틀 로얄.py
+++ 백준/Gold/32405. 배틀 로얄/배틀 로얄.py
@@ -1,31 +1,37 @@
+# 32405. 배틀로얄
 import sys
 from collections import deque
 input = sys.stdin.readline
 
-n = int(input())
-attacks = [0] + list(map(int, input().split()))
-healths = [0] + list(map(int, input().split()))
+N = int(input())
+d = [0] + list(map(int, input().split()))
+h = [0] + list(map(int, input().split()))
+q = deque(list(range(1, N+1)))
 
-q = deque()
-for i in range(1,n+1):
-    q.append(i)
-
+# 배틀 시작
 while len(q) > 1:
-    attacks_sum = 0
-    q_len = len(q)
     
-    for _ in range(q_len):
+    # 라운드별 남은 플레이어들의 공격력의 총합
+    sumd = 0
+    
+    # 살아남는 플레이어를 먼저 구하고, 그 공격력의 총합을 계산해둠
+    for _ in range(len(q)):
         i = q.popleft()
-        if healths[i] - attacks_sum >0:
-            attacks_sum += attacks[i]
-            q.append(i)
-            
-    q_len = len(q)
-    for _ in range(q_len):
+
+        if h[i] - sumd > 0:         # 앞까지의 플레이어가 모두 공격했을 때 살아남으면
+            sumd += d[i]            # 현재 공격력도 추가하고
+            q.append(i)             # q 끝에 다시 넣음
+
+    # hp는 이후 처리 
+    # : 살아남은 플레이어만 먼저 구해 그 공격력의 총합을 계산한 후 hp 계산함       
+    for _ in range(len(q)):
         i = q.popleft()
-        healths[i] -= (attacks_sum - attacks[i])
+        h[i] -= (sumd - d[i])       # 현재 플레이어들의 공격력의 총합에서 자기 자신 공격력을 빼고 모두 공격 처리
         
-        if healths[i] >0:
-            q.append(i)
+        if h[i] > 0:                # 자기자신을 제외한 모든 플레이어의 공격을 받고 살아남으면
+            q.append(i)             # q 끝에 다시 넣음
+        
+        if len(q) == 1:             # 모든 플레이어가 동시에 죽는 경우: 그럴 수는 없으니까 마지막 생존자는 살려줘야함
+            break
 
 print(q[0])
