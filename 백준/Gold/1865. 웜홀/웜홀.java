dc9e9c992ca196fe9bca3c75cd20a3d6df982933
jiwoo_ya <jweun1@soongsil.ac.kr>
2025-06-25 15:59:26 +0900

    [Gold III] Title: 웜홀, Time: 676 ms, Memory: 59164 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.java
+++ 백준/Gold/1865. 웜홀/웜홀.java
@@ -1,34 +1,46 @@
 /*
-* N개의 지점                            N 500
-* 사이에 M개의 도로와                     M 2500
-* W개의 웜홀 (방향O): 시간이 거꾸로 감      W 200
-* 각 시간 T 0~10^4
-* */
+ * N개의 지점                            N 500
+ * 사이에 M개의 도로와                     M 2500
+ * W개의 웜홀 (방향O): 시간이 거꾸로 감      W 200
+ * 각 시간 T 0~10^4
+ * */
 
 import java.util.*;
 
-class Node{
-    int s, e, t;
-    Node(int s, int e, int t){
-        this.s = s;
-        this.e = e;
-        this.t = t;
-    }
-}
+/*
+ * start를 지정하지 않고 음수 간선이 있는지만 파악하는 방식
+ * -> INF가 무한이 아닌 일반 큰 수라서 time만 비교하게 되고, update가 이뤄짐
+ * -> 통과는 하지만 비추
+ *
+ * -> 아예 INF를 설정하지 않고 time을 0으로 초기화한 후, N번째에 update 여부만 확인하는 방법도 있다. (안정적)
+ *
+ * start를 0번으로 지정하지 않을 거면
+ * for loop를 통해 모든 노드에 대해 고려하면서 true일 때 즉시 빠져나오는 방식으로 하는 게 정석적임 (시간은 더 오래 걸리긴 함)
+ * */
 
 public class Main {
 
-    static final int INF = 10_000_000;
+//    static final int INF = 10_000_000;
+
+    private static class Node{
+        int s, e, t;
+        Node(int s, int e, int t){
+            this.s = s;
+            this.e = e;
+            this.t = t;
+        }
+    }
 
     static boolean bellmanFord(int N, ArrayList<Node> nodes){
 
         int[] time = new int[N+1];
-        Arrays.fill(time, INF);
+//        Arrays.fill(time, INF);
         boolean updated = false;
 
-        for (int i=0; i<N-1; i++){
+        // N-1번 반복 (time[node.s] != INF 검사하지 않음)
+        for (int i=0; i<N; i++){
             for(Node node : nodes){
-                if (node.s != INF && time[node.s] + node.t < time[node.e]){
+                if (time[node.s] + node.t < time[node.e]){
                     time[node.e] = time[node.s] + node.t;
                     updated = true;
                 }
@@ -37,10 +49,9 @@ public class Main {
             if (!updated) return false;
         }
 
-//        Queue<Integer> affected = new LinkedList<>();
+        // 음수 사이클 있는지 판별
         for (Node node : nodes){
-            if (node.s != INF && time[node.s] + node.t < time[node.e]){
-//                affected.add(node.e);
+            if (time[node.s] + node.t < time[node.e]){
                 return true;
             }
         }
@@ -74,10 +85,7 @@ public class Main {
                 nodes.add(new Node(S, E, -T));
             }
 
-            boolean ans = bellmanFord(N, nodes);
-
-            if (ans) System.out.println("YES");
-            else System.out.println("NO");
+            System.out.println(bellmanFord(N, nodes)? "YES":"NO");
         }
     }
 }
