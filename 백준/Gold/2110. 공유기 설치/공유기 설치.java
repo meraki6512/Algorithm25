44e1a80682041390e5286feec601f9a1a6ed05e7
jiwoo_ya <jweun1@soongsil.ac.kr>
2025-08-22 13:32:36 +0900

    [Gold IV] Title: 공유기 설치, Time: 324 ms, Memory: 29928 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,6 +1,10 @@
+// package Algorithm.Algorithm25.Java.BOJ2110;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 import java.util.TreeSet;
 
@@ -16,8 +20,6 @@ import java.util.TreeSet;
 
 public class Main {
 
-    private static TreeSet<Long> houses;
-
     public static void main(String[] args) throws IOException {
         // In
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,27 +28,33 @@ public class Main {
         int C = Integer.parseInt(st.nextToken());
         long ans = 0;
 
-        houses = new TreeSet<>();
-        for (int i = 0; i < N; i++) houses.add(Long.parseLong(br.readLine()));
-        long l = 1;
-        long r = houses.last()-houses.first();
-
-        if (C==2) {
-            System.out.println(r);
-            return;
-        }
+        long[] houses = new long[N];
+        for (int i = 0; i < N; i++) houses[i] = Long.parseLong(br.readLine());
+        Arrays.sort(houses);
 
         // Sol
+        // 이분 탐색: 기준은 두 집 사이의 거리
+        // l (최소 거리: 1), r (최대 거리: 양 끝 집 거리)
+        long l = 1;
+        long r = houses[N-1]-houses[0];
+        if (C==2) {System.out.println(r); return;}
+
         while (l <= r){
             long m = (l+r)/2;
 
             // cnt: houses 1번 집인 곳부터 거리가 m 이상인 집에 설치할 때 총 설치 공유기 수
-            int cnt = 0;
-            for (Long prev = houses.first(); prev!=null; prev = houses.ceiling(prev+m))
-                cnt++;
+            int cnt = 1;
+            long prev = houses[0];
+            for (int i=1; i<N; i++){
+                // 거리 차가 m 이상인 경우만 공유기 설치
+                if (houses[i]-prev>=m){
+                    cnt++;
+                    prev = houses[i];
+                }
+            }
 
             // cnt (설치한 공유기 수), C (설치할 공유기 수)
-            if (cnt < C) r = m-1;
+            if (cnt < C) r = m - 1;
             else {l = m+1; ans=m;}
         }
 
@@ -54,4 +62,3 @@ public class Main {
         System.out.println(ans);
     }
 }
-
