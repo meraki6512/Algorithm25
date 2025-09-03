df8d2dd7c0f0a643253e1e73b0bf5b5ecb3108d1
meraki6512 <jweun123@gmail.com>
2025-09-03 23:47:59 +0900

    rm dir 0183

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++
@@ -1,4 +0,0 @@
-# Write your MySQL query statement below
-SELECT email from Person group by Email having count(email) > 1;
--- SELECT DISTINCT(p1.email) from Person p1, Person p2 where p1.id <> p2.id AND p1.email and p2.email;
--- SELECT DISTINCT(p1.email) from Person p1 JOIN Person p2 ON p1.email = p2.email AND p1.id <> p2.id;
