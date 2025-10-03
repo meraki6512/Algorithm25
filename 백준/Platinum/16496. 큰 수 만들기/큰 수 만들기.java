88c678510c59ad78ce4a524faa52214a9da0fcef
jiwoo_ya <jweun1@soongsil.ac.kr>
2025-10-03 20:54:41 +0900

    [Platinum V] Title: 큰 수 만들기, Time: 140 ms, Memory: 16472 KB -BaekjoonHub

--- 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.java
+++ 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.java
@@ -1,49 +1,30 @@
-// package Algorithm.Algorithm25.Java.BOJ16496;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
- * N개의 숫자 리스트에서 수를 나열해 만들 수 있는 가장 큰 수를 구하라.
- * (1 <= N <= 1000), (0 <= num <= 1_000_000_000)
+ * <a href="https://www.acmicpc.net/problem/16496">16496. 큰 수 만들기</a>
+ * <p>N개의 수를 나열해 만들 수 있는 가장 큰 수를 구하라.</p>
+ * <ul><li>1 <= N <= 1000</li><li>0 <= num <= 1_000_000_000</li></ul>
  */
 public class Main {
+
+	// -> 그냥 문자열 정렬 문제
+
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-
-		PriorityQueue<Node> nums = new PriorityQueue<>();
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) nums.add(new Node(st.nextToken()));
+		String[] nums = new String[N];
+		for(int i=0; i<N; i++) nums[i] = st.nextToken();
 
-		StringBuilder sb = new StringBuilder();
-		while(!nums.isEmpty()) sb.append(nums.poll().n);
+		Arrays.sort(nums, (s1, s2) -> (s2 + s1).compareTo(s1 + s2));
 
-		String ans = sb.toString();
-		System.out.println(ans.charAt(0) == '0' ? "0" : ans);
+		if (nums[0].equals("0")) System.out.println("0");
+		else System.out.println(String.join("", nums));
 
-	}
-}
-
-class Node implements Comparable<Node> {
-	String n;
-	public Node(String n) {
-		this.n = n;
-	}
-
-	@Override
-	public int compareTo(Node o) {
-		StringBuilder sbAB = new StringBuilder(), sbBA = new StringBuilder();
-
-		sbAB.append(n).append(o.n);
-		sbBA.append(o.n).append(n);
-
-		return sbBA.compareTo(sbAB); // DESC
 	}
 }
