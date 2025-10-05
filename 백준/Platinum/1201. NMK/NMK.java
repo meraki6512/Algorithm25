cde3fef58d9ea3ddbf50f69472e3bcce603746b6
jiwoo_ya <jweun1@soongsil.ac.kr>
2025-10-06 02:04:35 +0900

    Update NMK.java

--- 백준/Platinum/1201. NMK/NMK.java
+++ 백준/Platinum/1201. NMK/NMK.java
@@ -20,13 +20,15 @@ public class Main {
 		int K = sc.nextInt();
 
 		Solution sol = new Solution();
-		System.out.println(sol.solution(N, M, K));
+		// System.out.println(sol.solution(N, M, K));
+		System.out.println(sol.solution_other(N, M, K));
 	}
 }
 
 class Solution {
-	public String solution(int N, int M, int K) {
 
+	// 동적으로 그룹 내 숫자 수 LDS 이내로 계산함
+	public String solution(int N, int M, int K) {
 		if ((M + K - 1 > N) || (N > M * K))  return "-1"; // (M-1)+1+(K-1) <= N <= M*K
 
 		StringBuilder sb = new StringBuilder();
@@ -41,6 +43,31 @@ class Solution {
 			if (M == 0) break;
 
 			K = N/M;	// 다음 그룹의 수 개수 K 구하기
+		}
+
+		return sb.toString();
+	}
+
+	// 첫 그룹으로 LDS 보장 후 남은 개수로 그룹 수 계산부터 하고 분배함
+	public String solution_other(int N, int M, int K) {
+		if ((M + K - 1 > N) || (N > M * K))  return "-1"; // (M-1)+1+(K-1) <= N <= M*K
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = K; i > 0; i--) sb.append(i).append(" "); // 첫 그룹 먼저 만들기 (LDS: K)
+
+		int rN = N - K;		// 남은 숫자 수
+		int rM = M - 1;		// 남은 그룹 수
+		int x = K + 1;		// 다음 시작 숫자
+
+		if (rM > 0) {
+			int grp = rN / rM;	// 균등하게 분배했을 때, 각 그룹의 숫자 수
+			int rem = rN % rM;	// 나머지: 앞 그룹부터 하나씩 나눠줄 거
+
+			for (int i = 0; i < rM; i++) {
+				int curGrp = grp + (rem-- > 0 ? 1 : 0);	// (나머지가 있으면 개수 추가함) 현재 그룹의 숫자 수
+				x += curGrp;
+				for (int j = x - 1; j >= x - curGrp ; j--) sb.append(j).append(" "); // curGrp 크기만큼 감소하게 넣음
+			}
 		}
 
 		return sb.toString();
