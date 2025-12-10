5fde24a3f2354dc9f8cf173ec54ff01a782248c2
jiwoo_ya <jweun1@soongsil.ac.kr>
2025-12-10 19:38:21 +0900

    Time: 0 ms (100%), Space: 42.7 MB (44.96%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,18 @@
+class Solution {
+    private int[][] dp;
+
+    public int rob(int[] nums) {
+        int n = nums.length;
+        dp = new int[n][2];
+        dp[0][1] = nums[0];
+
+        // 0: not take
+        // 1: take
+        for (int i = 1; i < n ; i++) {
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1]);
+            dp[i][1] = dp[i-1][0] + nums[i];
+        }    
+
+        return Math.max(dp[n-1][0], dp[n-1][1]);
+    }
+}
