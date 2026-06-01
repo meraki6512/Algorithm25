b29aec9b1268b37eb34661c8579b7e8853cb49e7
jiwoo_ya <jweun1@soongsil.ac.kr>
2026-06-01 22:30:56 +0900

    [level 2] Title: 타겟 넘버, Time: 4.05 ms, Memory: 64.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,25 @@
+class Solution {
+    
+    private int[] numbers;
+    private int target, answer = 0;
+    
+    public int solution(int[] Numbers, int Target) {
+        numbers = Numbers;
+        target = Target;
+        
+        dfs(0, 0);
+        
+        return answer;
+    }
+    
+    private void dfs(int idx, int sum) {
+        
+        if (idx == numbers.length) {
+            if (sum == target) answer ++;
+            return;
+        }
+        
+        dfs(idx+1, sum + numbers[idx]);
+        dfs(idx+1, sum - numbers[idx]);
+    }
+}
